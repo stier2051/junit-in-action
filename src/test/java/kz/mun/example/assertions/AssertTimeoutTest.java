674f4e46654a256f4ae53a0df26c3890afd3052e
Alexandr Mun <mun_a2006@yahoo.de>
2023-10-10 09:27:22 +0600

    ch2_assertions_2

---
+++ src/test/java/kz/mun/example/assertions/AssertTimeoutTest.java
@@ -0,0 +1,41 @@
+package kz.mun.example.assertions;
+
+import kz.mun.example.ch1.assertions.Job;
+import kz.mun.example.ch1.assertions.NoJobException;
+import kz.mun.example.ch1.assertions.SUT;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssertTimeoutTest {
+    private final SUT systemUnderTest = new SUT("Our system undertest");
+
+    @Test
+    @DisplayName("A job is executed within a timeout")
+    void testTimeout() throws InterruptedException {
+        systemUnderTest.addJob(new Job("Job 1"));
+        assertTimeout(ofMillis(500), () -> systemUnderTest.run(200));
+    }
+
+    @Test
+    @DisplayName("A job is executed preemptively within a timeout")
+    void testTimeoutPreemptively() throws InterruptedException {
+        systemUnderTest.addJob(new Job("Job 1"));
+        assertTimeoutPreemptively(ofMillis(500), () -> systemUnderTest.run(200));
+    }
+
+    @Test
+    @DisplayName("An Exception is expected")
+    void testExpectedException() {
+        assertThrows(NoJobException.class, systemUnderTest::run);
+    }
+
+    @Test
+    @DisplayName("An exception is caught")
+    void testCatchException() {
+        Throwable throwable = assertThrows(NoJobException.class, () -> systemUnderTest.run(1000));
+        assertEquals("No jobs on the execution list!", throwable.getMessage());
+    }
+}
